0c5a727adb2694b0432b67a58ba1e0ebf73d024b
Leonel Ison <m.ison@itecriocuarto.org.ar>
2024-07-25 19:20:37 -0300

    Modelos creados y autos agregados a la base de datos con el csv proveído por el profe

--- gestion_autos/admin.py
+++ gestion_autos/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Marca, TipoCombustible, ModeloAuto, Auto
 
-# Register your models here.
+@admin.register(Marca)
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
+@admin.register(TipoCombustible)
+class TipoCombustibleAdmin(admin.ModelAdmin):
+    list_display = ('tipo',)
+
+@admin.register(ModeloAuto)
+class ModeloAutoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'marca',)
+    search_fields = ('nombre', 'marca__nombre',)
+    list_filter = ('marca',)
+
+@admin.register(Auto)
+class AutoAdmin(admin.ModelAdmin):
+    ordering = ('modelo__nombre', 'año_fabricacion',)
+    search_fields = ('modelo__nombre', 'año_fabricacion', 'tipo_combustible__tipo',)
+    list_filter = ('modelo__marca', 'tipo_combustible',)
+    empty_value_display = 'No disponible'
+
+    list_display = (
+        'modelo',
+        'año_fabricacion',
+        'cantidad_puertas',
+        'cilindrada',
+        'tipo_combustible',
+        'pais_fabricacion',
+        'precio_dolares',
+        'get_precio_en_colores',
+    )
+
+    def get_precio_en_colores(self, obj):
+        ALTO = '#FF0000'
+        MEDIO = '#FFD700'
+        BAJO = '#008F39'
+        color = ''
+
+        if obj.precio_dolares > 50000:
+            color = ALTO
+        elif 20000 <= obj.precio_dolares <= 50000:
+            color = MEDIO
+        else:
+            color = BAJO
+
+        return format_html(
+            '<span style="color: {}">${}</span>', color, obj.precio_dolares
+        )
+
+    fieldsets = [
+        (
+            "Información del Auto",
+            {
+                "fields": ["modelo", "año_fabricacion", "cantidad_puertas", "cilindrada", "tipo_combustible", "pais_fabricacion", "precio_dolares"],
+            },
+        ),
+    ]
+
+
--- gestion_autos/models.py
+++ gestion_autos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class TipoCombustible(models.Model):
+    tipo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tipo
+
+class ModeloAuto(models.Model):
+    nombre = models.CharField(max_length=100)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.marca} {self.nombre}'
+
+class Auto(models.Model):
+    modelo = models.ForeignKey(ModeloAuto, on_delete=models.CASCADE)
+    año_fabricacion = models.IntegerField()
+    cantidad_puertas = models.IntegerField()
+    cilindrada = models.FloatField()
+    tipo_combustible = models.ForeignKey(TipoCombustible, on_delete=models.CASCADE)
+    pais_fabricacion = models.CharField(max_length=100)
+    precio_dolares = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.modelo} ({self.año_fabricacion})'
