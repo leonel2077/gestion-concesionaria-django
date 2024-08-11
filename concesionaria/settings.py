f195545afddc87ba9af7ca591df293c893e8fa9f
Leonel Ison <m.ison@itecriocuarto.org.ar>
2024-08-11 15:33:31 -0300

    context processors añadidos

--- concesionaria/settings.py
+++ concesionaria/settings.py
@@ -65,6 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'gestion_autos.context_processors.total_autos',
+                'gestion_autos.context_processors.all_marcas',
             ],
         },
     },
